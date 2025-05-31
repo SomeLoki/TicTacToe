3861553863b482b86535bac81b61b1b75c1c4f53
someloki <tenacity686@gmail.com>
2025-05-31 02:37:45 -0700

    Feat: Add win con logic + row + col, - diag

--- script.js
+++ script.js
@@ -12,32 +12,34 @@ function createPlayer (name, mark) {
 }  
 
 const makePlayers = (function() {
+// create two players and add to the gameboard
   const playerOne = createPlayer("PlayerOne", "O");
-  const playerTWo = createPlayer("playerTwo", "X");
+  const playerTwo = createPlayer("playerTwo", "X");
 
   gameboard.playerOne = playerOne;
-  gameboard.playerTWo = playerTWo;
+  gameboard.playerTWo = playerTwo;
 
   console.log(gameboard);
   console.log(gameboard.playerOne);
-  console.log(gameboard.playerTWo);
+  console.log(gameboard.playerTwo);
 
 })();
 
 function createGameboardSquare (row, column) {
-  const name = `row${row}col${column}`;
+  const squareId = `row${row}col${column}`;
 
   let currentMark = "";
   const getCurrentMark = () => currentMark;
+  // check if there is an existing mark before changing it
   const changeCurrentMark = (newMark) => (!currentMark) ? currentMark = newMark : alert("Invalid choice");
   const resetCurrentMark = () => currentMark = "";
 
-  return { name, row, column, getCurrentMark, changeCurrentMark, resetCurrentMark };
+  return { squareId, row, column, getCurrentMark, changeCurrentMark, resetCurrentMark };
 
 }
 
 const makeGameBoard = (function() {
-
+// Create a 3x3 gameboard with squares and add to gameboard
   const gamesquares = [];
 
   for ( let row = 1; row < 4; row++ ) {
@@ -45,9 +47,31 @@ const makeGameBoard = (function() {
       gamesquares.push(createGameboardSquare( row, column ));
     }
   }
-  gameboard.gamesquares = gamesquares;
+  gameboard.squares = gamesquares;
   console.log(gameboard);
 })();
 
+const makeWinStates = ( function() {
+  // check if all 3 in the same row or column have the same mark
+  const checkLineWin = ( marked, whereToCheck ) => {
+    const matchingSquares = gameboard.squares.filter( ( square ) => square[whereToCheck] === marked );
+    // must have 3 squares, none of them empty
+    if (matchingSquares.length !== 3 || matchingSquares.some(square => square.getCurrentMark() === "")) {
+      return false;
+    }
+    const firstMark = matchingSquares[0].getCurrentMark();
+    return matchingSquares.every(square => square.getCurrentMark() === firstMark);
+  };
 
+  // both of these just reuse checkLineWin, but for readability they are separate calls.
+  const checkRowWin = ( markedRow )  => {
+    return checkLineWin( markedRow, "row" );
+  };
 
+  const checkColumnWin = ( markedColumn )  => {
+    return checkLineWin( markedColumn, "column" );
+  };
+  // check the two diagonal for the same mark
+
+  gameboard.checkWinStates = { checkRowWin, checkColumnWin };
+})();
